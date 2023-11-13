cab77bb880fc68de5a72f256e87618caefa0bdeb
Steve Thomson <steve@panda.sh>
2023-11-13 21:35:49 +0000

    css and prettier

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@netlify/remix-runtime";
+import type { LinksFunction } from '@netlify/remix-runtime'
+import { cssBundleHref } from '@remix-run/css-bundle'
 import {
   Links,
   LiveReload,
@@ -7,27 +7,41 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
+
+import './styles/pandastyle/pandastyle-globals.css'
+import './styles/pandastyle/pandastyle-theme-happyhues.css'
+import './styles/pandastyle/pandastyle-code.css'
+import './styles/pandastyle/pandastyle-elements.css'
+import './styles/pandastyle/pandastyle-layout.css'
+import './styles/pandastyle/pandastyle-stacks.css'
+import './styles/pandastyle/pandastyle-typography.css'
+import './styles/pandastyle/pandastyle-forms.css'
+import './styles/pandastyle/pandastyle-cards.css'
+import './styles/pandastyle/pandastyle-decorations.css'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+]
 
 export default function App() {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className="🐼-theme-happyhues-palettes-base 🐼-theme-happyhues-sizing-base 🐼-html"
+    >
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="🐼-body">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@ import { config } from '@netlify/remix-edge-adapter'
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ...config,
+  // serverDependenciesToBundle: ['netlify-cli', 'ansi-align', 'string-width'],
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
   //
