378ef1fc6b0c3563bfc2358f2f1a490a83f63e78
Steve Thomson <steve@panda.sh>
2024-11-06 22:22:23 +0000

    finish february

--- app/routes/2024.february.tsx
+++ app/routes/2024.february.tsx
@@ -88,7 +88,7 @@ export default function Index() {
             The problem with alignment
           </h2>
           <p className="🐼-section-item 🐼-text">
-            In the world of <a href="https://threejs.org">three.js</a>{' '}
+            In the world of <Anchor href="https://threejs.org">three.js</Anchor>{' '}
             everything is aligned centred on each axis by default. This means
             that if you place an object at a position of interest it will likely
             overlap something else in a way you did not intend it to. In the
@@ -108,42 +108,114 @@ export default function Index() {
 
       <div className="🐼-section">
         <div className="🐼-section-page 🐼-section-text">
+          <h2 className="🐼-section-item 🐼-heading">Getting aligned</h2>
+          <p className="🐼-section-item 🐼-text">
+            To fix this I&apos;m using an <Code>{'<Alignment />'}</Code>{' '}
+            component and <Code>{`withAxisAlignment`}</Code> hook that can shift
+            the position of a child component to align it how you want
+          </p>
+
           <CodeBlock>{`
-import * as React from "react";
-import { HelloComponent } from "./hello";
-import { NameEditComponent } from "./nameEdit";
-
-export const App = () => {
-  const [name, setName] = React.useState("defaultUserName");
-  const [editingName, setEditingName] = React.useState("defaultUserName");
-
-  const loadUsername = () => {
-    setTimeout(() => {
-      setName("name from async call");
-      setEditingName("name from async call");
-    }, 500);
-  };
-
-  React.useEffect(() => {
-    loadUsername();
-  }, []);
-
-  const setUsernameState = () => {
-    setName(editingName);
-  };
+const ExampleTwo = () => {
+  const { CustomProperties, customProperties } = useCustomProperties([
+    '--🎨-background',
+    '--🎨-art-accent',
+  ])
 
   return (
-    <>
-      <HelloComponent userName={name} />
-      <NameEditComponent
-        initialUserName={\`hey\${yo}hey\`}
-        editingName={editingName}
-        onNameUpdated={setUsernameState}
-        onEditingNameUpdated={setEditingName}
-      />
-    </>
-  );
-};
+    <CustomProperties>
+      <ExampleScene>
+        <DebugBox
+          color={customProperties?.['--🎨-background'] || 'green'}
+          position={new Vector3(0, 0, 0)}
+          size={[0.5, 0.5, 0.5]}
+        />
+
+        <Alignment
+          x={AxisAlignment.CENTER}
+          y={AxisAlignment.CENTER}
+          z={AxisAlignment.CENTER}
+        >
+          <DebugBox
+            color={customProperties?.['--🎨-art-accent'] || 'red'}
+            position={new Vector3(0.25, 0.25, 0.25)}
+            size={[0.5, 0.5, 0.5]}
+          />
+        </Alignment>
+      </ExampleScene>
+    </CustomProperties>
+  )
+}
+          `}</CodeBlock>
+
+          <CodeBlock>{`
+const Alignment = ({
+  children,
+  x = AxisAlignment.CENTER,
+  y = AxisAlignment.CENTER,
+  z = AxisAlignment.CENTER,
+  offsets,
+}: AlignmentProps) => {
+  if (!isValidElement(children)) {
+    throw new Error('Alignment requires a single React element as child')
+  }
+
+  const { position = new Vector3(0, 0, 0), size } = children.props
+
+  if (!size) {
+    throw new Error('Child component must have a size prop')
+  }
+
+  const alignedPosition = withAxisAlignment({
+    position,
+    size,
+    x,
+    y,
+    z,
+    offsets,
+  })
+
+  return cloneElement(children, {
+    position: alignedPosition,
+  })
+}
+          `}</CodeBlock>
+
+          <CodeBlock>{`
+const withAxisAlignment = ({
+  position = new Vector3(0, 0, 0),
+  size,
+  x = AxisAlignment.CENTER,
+  y = AxisAlignment.CENTER,
+  z = AxisAlignment.CENTER,
+  offsets = new Vector3(0, 0, 0),
+}: {
+  position?: Vector3
+  size: BoxSize
+  x?: AxisAlignment
+  y?: AxisAlignment
+  z?: AxisAlignment
+  offsets?: Vector3
+}) => {
+  const [width, height, depth] = size
+  const positionCopy = position.clone()
+
+  // Center alignment is the default
+  let alignedX = 0
+  let alignedY = 0
+  let alignedZ = 0
+
+  if (x === AxisAlignment.START) alignedX = width / 2
+  if (x === AxisAlignment.END) alignedX = -width / 2
+  if (y === AxisAlignment.START) alignedY = -height / 2
+  if (y === AxisAlignment.END) alignedY = height / 2
+  if (z === AxisAlignment.START) alignedZ = -depth / 2
+  if (z === AxisAlignment.END) alignedZ = depth / 2
+
+  return positionCopy
+    .add(new Vector3(alignedX, alignedY, alignedZ))
+    .add(offsets)
+}
           `}</CodeBlock>
         </div>
       </div>
